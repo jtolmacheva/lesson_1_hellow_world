799206faeb797cc7518f10e8b9d31fa91c44cca6
tolmacheva <tolmacheva.july920506@gmail.com>
2024-04-04 19:27:04 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 2;
+int b = 7;
+int c = 21;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
+
